a1de720fc51887f17f5b3041c505ee40ce5189ea
Jacksparrow7-coder <sugishiva0@gmail.com>
2026-01-11 19:50:33 +0530

    started building

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Spline from '@splinetool/react-spline/next';
+const Header = () => {
+  return (
+    <div>
+       <Spline
+        scene="https://prod.spline.design/DHIHVtKrYdkZqpg9/scene.splinecode" 
+      />
+    </div>
+  )
+}
+
+export default Header
